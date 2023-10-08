455750b843a8d9f758497b47eb4e1ff0c0344848
Mani Sundararajan <mani@hey.com>
2023-10-08 16:44:23 -0500

    Switch to using clap. Add TODOs

--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,87 @@
-use bpaf::Bpaf;
+use clap::{ArgGroup, Parser};
 use color_eyre::eyre::Result;
-use indicatif;
-use std::process;
+use indicatif::{self, ProgressBar};
 use std::thread;
-use std::time;
+use std::time::Duration;
 
-#[derive(Clone, Debug, Bpaf)]
-#[bpaf(options, version)]
+use console::Term;
+
+#[derive(Debug, Parser)]
+#[command(version, author, about, long_about = None, group = ArgGroup::new("duration").required(true))]
 /// An entirely Rust based Pomodoro timer
-struct Opts {
+pub struct Args {
     /// 10 minutes
-    pub long: bool,
+    #[arg(short, long, group = "duration")]
+    long: bool,
 
     /// 5 minutes
-    pub short: bool,
+    #[arg(short, long, group = "duration")]
+    short: bool,
 
     /// Specify your own time in minutes
-    pub time: usize,
+    #[arg(short, long, group = "duration")]
+    time: usize,
 }
 
 #[derive(Debug)]
-pub struct Config {
+pub struct Timer {
     pub time_min: usize,
     pub time_sec: usize,
     pub current_time: usize,
 }
 
-impl Config {
-    // pub fn new(opts: Opts) -> Result<Config> {
-    //     let time_minutes = opts::Ti
+impl Timer {
+    pub fn new(args: Args) -> Self {
+        let time_min = if args.long {
+            10
+        } else if args.short {
+            5
+        } else {
+            args.time
+        };
 
-    //     let time_sec: usize = 60 * time_min;
-    //     let current_time = 0;
+        let time_sec: usize = 60 * time_min;
+        let current_time = 0;
 
-    //     Ok(Config {
-    //         time_min,
-    //         time_sec,
-    //         current_time,
-    //     })
-    // }
+        Timer {
+            time_min,
+            time_sec,
+            current_time,
+        }
+    }
 }
 
 fn main() -> Result<()> {
     color_eyre::install()?;
-    println!("Welcome to Pomo-rs");
+    let term = Term::stdout();
+    term.write_line("Welcome to Pomo-rs")?;
 
-    println!("{:?}", opts().run());
+    /*
+    TODO: Display KeyPress commands to the user. This should be permanently displayed
+    */
 
-    // let config = Config::new(opts().run()).unwrap_or_else(|err| {
-    //     eprintln!("{}", err);
-    //     process::exit(1);
-    // });
+    let mut timer = Timer::new(Args::parse());
+    // println!("{:?}", timer);
 
     // println!(
     //     "Pomodoro Time in min: {}; in seconds: {}; current time: {}",
     //     config.time_min, config.time_sec, config.current_time
     // );
 
-    // let pb = indicatif::ProgressBar::new(config.time_min as u64);
-    // pb.set_style(
-    //     indicatif::ProgressStyle::default_bar()
-    //         .template("[{elapsed_precise}] {wide_bar:.green/red} [{eta_precise}] {msg}")?,
-    // );
+    let pb = ProgressBar::new(timer.time_min as u64);
+    pb.set_style(
+        indicatif::ProgressStyle::default_bar()
+            .template("[{elapsed_precise}] {wide_bar:.green/red} [{eta_precise}] {msg}")?,
+    );
 
-    // for _ in 0..=config.time_min {
-    //     thread::sleep(time::Duration::from_secs(1));
-    //     pb.inc(1);
-    // }
-    // pb.finish_with_message("done");
+    // TODO: loop for processing commands by user
+
+    // Why loop per minute? maybe should loop per second?
+    for _ in 0..=timer.time_min {
+        thread::sleep(Duration::from_millis(1000));
+        pb.inc(1);
+    }
+    pb.finish_with_message("done");
 
     Ok(())
 }
